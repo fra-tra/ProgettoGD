60a65aca99db6876a47bfdd2a2ffbe3a29fd863e
MarikaMontalto <marika.montalto@gmail.com>
2021-05-16 13:27:07 +0200

    Hammer code pt.1

--- ProgettoGD/Assets/Scripts/SpecialObjects.cs
+++ ProgettoGD/Assets/Scripts/SpecialObjects.cs
@@ -14,10 +14,15 @@ public class SpecialObjects : MonoBehaviour
     //globe 5
     //gear 6
 
+    //Questo script viene attaccato al Third Person Controller
+
     public int _currentLevel;
     public int _firstObject;
     public int _secondObject;
+
     [SerializeField] private LevelLoader _levelLoader;
+    //[SerializeField] private float _minDistanceHammer;
+
     private Counter _myCounter;
    
     void Start()
@@ -39,33 +44,33 @@ public class SpecialObjects : MonoBehaviour
 
         if( _currentLevel == 3) //Poesia Epica
         {
-            if(_firstObject == 1 | _secondObject == 1 ) //martello
+            if(_firstObject == 1) //martello
             {
                 useHammer();//Funzionamento di x
             }
-            else if(_firstObject == 2 | _secondObject ==2 ) //chiave
+            else if(_secondObject ==2) //chiave
             {
                 useKey();//Funzionamento di x
             }
         }
         else if(_currentLevel == 7) //Astronomia
         {
-            if(_firstObject == 5| _secondObject == 5) //globo
+            if(_firstObject == 5) //globo
             {
                 useGlobe();//Funzionamento di x
             }
-            else if(_firstObject == 6| _secondObject == 6) //ingranaggio
+            else if(_secondObject == 6) //ingranaggio
             {
                 useGear();//Funzionamento di x
             }
         }
         else if(_currentLevel == 6) //Commedia
         {
-            if(_firstObject == 3| _secondObject == 3) //edera
+            if(_firstObject == 3) //edera
             {
                 useIvy();//Funzionamento di x
             }
-            else if(_firstObject == 4 | _secondObject == 4) //fionda
+            else if(_secondObject == 4) //fionda
             {
                 useSling();//Funzionamento di x
             }
@@ -75,32 +80,44 @@ public class SpecialObjects : MonoBehaviour
 
     public void useHammer()
     {
-        Debug.Log("Special Object");
+        Debug.Log("Special Object Hammer");
+       /* if (IsTargetWithinDistance(_minDistanceHammer))
+        {
+            
+        }*/
+
+        RaycastHit hit;
+
+        if(Physics.Raycast(transform.position, Vector3.forward, out hit))
+        {
+            var floortag = hit.collider.gameObject.tag;
+        }
+
     }
 
     public void useKey()
     {
-        Debug.Log("Special Object");
+        Debug.Log("Special Object Key");
     }
 
     public void useGlobe()
     {
-        Debug.Log("Special Object");
+        Debug.Log("Special Object Globe");
     }
 
     public void useGear()
     {
-        Debug.Log("Special Object");
+        Debug.Log("Special Object Gear");
     }
 
     public void useIvy()
     {
-        Debug.Log("Special Object");
+        Debug.Log("Special Object Ivy");
     }
 
     public void useSling()
     {
-        Debug.Log("Special Object");
+        Debug.Log("Special Object Sling");
     }
 
 
@@ -113,5 +130,10 @@ public class SpecialObjects : MonoBehaviour
             UseSpecialObject();
         }
     }
+
+    /*private bool IsTargetWithinDistance(float distance)
+    {
+        return (_target.transform.position - transform.position).sqrMagnitude <= distance * distance;
+    }*/
 
 }
