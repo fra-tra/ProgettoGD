c0e9b4def6547006ccc4f2684328db5b23263e20
MarikaMontalto <marika.montalto@gmail.com>
2021-05-20 11:04:35 +0200

    Rocks script on Poesia Epica Scene

--- ProgettoGD/Assets/Scripts/RocksGenerator.cs
+++ ProgettoGD/Assets/Scripts/RocksGenerator.cs
@@ -12,7 +12,8 @@ public class RocksGenerator : MonoBehaviour
     [SerializeField] private Collider _space; //Il collider che si decide di passargli
 
     private Coroutine _coroutine;
-    private bool entered = false;
+    private bool _falling = false;
+    private bool _rightTime = true;
 
     // Start is called before the first frame update
     void Start()
@@ -23,36 +24,48 @@ public class RocksGenerator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (_falling && _rightTime)
+        {
+            _coroutine = StartCoroutine(MyCoroutine());
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            _falling = false;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Entered");
-        if (!entered)
+        //Debug.Log("Entered");
+        if (other.tag == "Player")
         {
-            entered=true;
-            SpawnRocks();
+            _falling = true;
         }
-        //_coroutine = StartCoroutine(MyCoroutine());
     }
 
-
-    /*public IEnumerator MyCoroutine()
+    public IEnumerator MyCoroutine()
     {
         Debug.Log("In corout");
         SpawnRocks();
-        yield return new WaitForSeconds(Random.Range(10000f , 20000f));
+        _rightTime = false;
+        yield return new WaitForSeconds(Random.Range(1f , 5f));
         Debug.Log("After Yield");
+        _rightTime = true;
         StopCoroutine(_coroutine);
     }
-*/
+
     public void SpawnRocks()
     {
-        for (int i = 0; i < _rocksToSpawn; i++)
+        int max = (int)Random.Range(1, _rocksToSpawn);
+        for (int i = 0; i < max; i++)
         {
             Debug.Log("Spawn");
-            Instantiate(_rockPrefab, GetRandomPosition(), Quaternion.identity);
+            Object obj = Instantiate(_rockPrefab, GetRandomPosition(), Quaternion.identity);
+            Destroy(obj, 5);
         }
     }
 
