089634c6f37fc743d307d439331693f73a4e803f
MarikaMontalto <marika.montalto@gmail.com>
2021-05-30 23:06:30 +0200

    NavAgents che esplodono Unity

---
+++ ProgettoGD/Assets/Scripts/NavAgentSpawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavAgentSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _navAgentPrefab;
+    [SerializeField] private int _navMeshAgentsToSpawn = 10;
+    [SerializeField] private Collider _groundCollider;
+    [SerializeField] private float _altezzaTerreno = 0f;
+
+    private static NavAgentSpawner _instance;
+
+    public static NavAgentSpawner Instance => _instance;
+
+    void Start()
+    {
+        Debug.Log("Spawned");
+        _instance = this;
+        SpawnNavMeshAgents();
+    }
+
+    private void SpawnNavMeshAgents()
+    {
+        for (int i = 0; i < _navMeshAgentsToSpawn; i++)
+        {
+            Debug.Log("Hello I'm navmesh");
+            GameObject agent = Instantiate(_navAgentPrefab, GetRandomPositionOnGround(), Quaternion.identity);
+            NavAgentStroll targetReached = agent.GetComponent<NavAgentStroll>();
+        }
+    }
+
+    
+    public Vector3 GetRandomPositionOnGround()
+    {
+        Vector3 min = _groundCollider.bounds.min;
+        Vector3 max = _groundCollider.bounds.max;
+        return new Vector3(Random.Range(min.x, max.x), _altezzaTerreno, Random.Range(min.z, max.z)); //_altezzaTerreno è la misura per farlo stare poggiato coi piedi sul ground
+    }
+}
---
+++ ProgettoGD/Assets/Scripts/NavAgentStroll.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavAgentStroll : MonoBehaviour
+{
+    private UnityEngine.AI.NavMeshAgent _navMeshAgent;
+
+    void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        SetDestination();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(DestinationReached())
+            SetDestination();
+    }
+
+    private void SetDestination()
+    {
+        UnityEngine.AI.NavMeshPath path = new UnityEngine.AI.NavMeshPath();
+        Vector3 randomPosition = NavAgentSpawner.Instance.GetRandomPositionOnGround();
+        
+        while (!_navMeshAgent.CalculatePath(randomPosition, path))
+        {
+            randomPosition = NavAgentSpawner.Instance.GetRandomPositionOnGround();
+        }  
+        _navMeshAgent.SetDestination(randomPosition);
+        
+    }
+
+    private bool DestinationReached()
+    {
+        if (!_navMeshAgent.pathPending)
+            if (_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance)
+                if (!_navMeshAgent.hasPath || _navMeshAgent.velocity.sqrMagnitude <= 0f)
+                    {
+                        return true;
+                    }
+                    
+        return false;
+    }
+}
