c0e11e41a128ed1203597793eee1c94cf95db6d1
MarikaMontalto <marika.montalto@gmail.com>
2021-06-05 23:09:22 +0200

    Ammazzo tutti quelli che hanno inventato la fisica

---
+++ ProgettoGD/Assets/scriptTry.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scriptTry : MonoBehaviour
+{
+    [SerializeField] GameObject _player;
+    // Start is called before the first frame update
+    void Start()
+    {
+         _player.transform.parent = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
