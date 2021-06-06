779b1495f254ced63e6a41f219b842678cdd01d4
fra-tra <francesca.trainiti@studenti.polito.it>
2021-06-06 10:12:08 +0200

    tentativi di rotation e parenting

--- ProgettoGD/Assets/scriptTry.cs
+++ ProgettoGD/Assets/scriptTry.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class scriptTry : MonoBehaviour
 {
     [SerializeField] GameObject _player;
+    [SerializeField] GameObject _Cube;
     // Start is called before the first frame update
     void Start()
     {
-         _player.transform.parent = transform;
+        _player.transform.SetParent(this.transform,true);
+        _Cube.transform.SetParent(this.transform,true);
     }
+    
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(new Vector3(0, 10f * Time.deltaTime, 0));
     }
 }
