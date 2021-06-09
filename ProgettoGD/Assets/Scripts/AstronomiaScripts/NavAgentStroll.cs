aa21b5b62afcfc022f964a039d07c4fe7023e62b
MarikaMontalto <marika.montalto@gmail.com>
2021-06-09 23:51:22 +0200

    Nav agents aiuto

--- ProgettoGD/Assets/Scripts/AstronomiaScripts/NavAgentStroll.cs
+++ ProgettoGD/Assets/Scripts/AstronomiaScripts/NavAgentStroll.cs
@@ -8,26 +8,31 @@ public class NavAgentStroll : MonoBehaviour
     private UnityEngine.AI.NavMeshAgent _navMeshAgent;
     [SerializeField] private Collider _groundCollider;
     [SerializeField] private GameObject _target;
-    [SerializeField] private float _minChaseDistance = 3f;
+    [SerializeField] private float _minChaseDistance = 1f;
     private bool _Patrolling = true;
 
     void Start()
     {
+        Debug.Log("Start");
         _navMeshAgent = GetComponent<NavMeshAgent>();
         _navMeshAgent.SetDestination( GetRandomPositionOnGround() );
+
     }
 
     void Update()
     {
+
         if ( DistanceFromTarget() <= _minChaseDistance)
         {
             _Patrolling = false;
-                ChaseTarget();
+            ChaseTarget();
         }
-        else
+       else
         {
-             if( DestinationReached() && _Patrolling)
+            if( DestinationReached() && _Patrolling)
+            {
                 SetNewDestination();
+            }
         }
             
     }
@@ -53,10 +58,12 @@ public class NavAgentStroll : MonoBehaviour
 
     private bool DestinationReached()
     {
+        
         if (!_navMeshAgent.pathPending)
             if (_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance)
                 if (!_navMeshAgent.hasPath || _navMeshAgent.velocity.sqrMagnitude <= 0f)
                     {
+                        Debug.Log("navigation Reached");
                         return true;
                     }
                     
@@ -65,9 +72,10 @@ public class NavAgentStroll : MonoBehaviour
 
     public Vector3 GetRandomPositionOnGround()
     {
+        Debug.Log("random position");
         Vector3 min = _groundCollider.bounds.min;
         Vector3 max = _groundCollider.bounds.max;
-        return new Vector3(Random.Range(min.x, max.x), 0.1f, Random.Range(min.z, max.z)); //_altezzaTerreno è la misura per farlo stare poggiato coi piedi sul ground
+        return new Vector3(Random.Range(min.x, max.x), 1f, Random.Range(min.z, max.z)); //_altezzaTerreno è la misura per farlo stare poggiato coi piedi sul ground
     }
 
     private float DistanceFromTarget() => Vector3.Distance(_target.transform.position, transform.position);
