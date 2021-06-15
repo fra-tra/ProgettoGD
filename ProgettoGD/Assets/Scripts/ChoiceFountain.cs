7ad882f6479aac2bce4492297d3027138fc71498
fra-tra <francesca.trainiti@studenti.polito.it>
2021-06-15 16:38:46 +0200

    CutScene Final Hub + scelta fontane con glow

---
+++ ProgettoGD/Assets/Scripts/ChoiceFountain.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class ChoiceFountain : MonoBehaviour
+{
+    [SerializeField] GameObject _fountain; 
+    [SerializeField] Material _matFountainGlow;
+    // Start is called before the first frame update
+    [SerializeField] public VideoPlayer _video;
+    [SerializeField] public GameObject _videoPanel;
+    [SerializeField] public LevelLoader _levelLoader;
+    [SerializeField] public GameObject _post;
+
+    private Material _matF;
+    private Coroutine _coroutine;
+    private bool _notChoosen = true;
+    private bool _memoryPlayed = false;
+    public float transitionTime = 1f;
+
+    void Start()
+    {
+        _matF= _fountain.GetComponent<MeshRenderer> ().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter()
+    {
+        _fountain.GetComponent<MeshRenderer> ().material = _matFountainGlow;
+        _post.SetActive(true);
+    }
+
+    private void OnTriggerExit()
+    {
+        _fountain.GetComponent<MeshRenderer> ().material = _matF;
+        _post.SetActive(false);
+    }
+
+    private void OnTriggerStay()
+    {
+        CheckChoice();
+    }
+
+    private void CheckChoice()
+    {
+        if(Input.GetButton("SpecialObject") && _notChoosen)
+        {
+            _notChoosen = false;
+            _coroutine = StartCoroutine(CoroutinePlayVideo());
+        }
+    }
+
+    public IEnumerator CoroutinePlayVideo()
+    {   Debug.Log("ChoiceVideo");
+        _videoPanel.SetActive(true);
+        _video.playOnAwake = false;
+
+        _video.Play();
+        transitionTime = (float) _video.length;
+        yield return new WaitForSeconds(transitionTime + 0.1f);
+        
+        _videoPanel.SetActive(false);
+        StopCoroutine(_coroutine);
+        _levelLoader.ToMenu();
+    }
+}
---
+++ ProgettoGD/Assets/Scripts/CutSceneSequence.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutSceneSequence : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] GameObject Cam1;
+    [SerializeField] GameObject Cam2;
+    [SerializeField] GameObject Cam3;
+    [SerializeField] GameObject Cam4;
+    [SerializeField] GameObject Cam5;
+    [SerializeField] GameObject CamPlayer;
+    [SerializeField] AudioSource _source;
+
+    [SerializeField] GameObject CutSceneEmpty;
+
+    private Coroutine _coroutine;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void OnTriggerEnter()
+    {
+        _source.Play();
+
+        _coroutine = StartCoroutine(Sequence());
+
+    }
+
+    IEnumerator Sequence()
+    {
+        Cam1.SetActive(true);
+        CamPlayer.SetActive(false);
+        yield return new WaitForSeconds(9.5f);
+        Cam2.SetActive(true);
+        Cam1.SetActive(false);
+        yield return new WaitForSeconds(1f);
+        Cam3.SetActive(true);
+        Cam2.SetActive(false);
+        yield return new WaitForSeconds(1.8f);
+
+        Cam4.SetActive(true);
+        Cam3.SetActive(false);
+        yield return new WaitForSeconds(1.7f);
+        Cam5.SetActive(true);
+        Cam4.SetActive(false);
+        yield return new WaitForSeconds(2.4f);
+        CamPlayer.SetActive(true);
+        Cam5.SetActive(false);
+        yield return new WaitForSeconds(2.5f);
+        StopCoroutine(_coroutine);
+        CutSceneEmpty.SetActive(false);
+    }
+}
