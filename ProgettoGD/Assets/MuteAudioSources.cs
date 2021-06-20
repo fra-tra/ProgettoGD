8209def9ccfa741e3026c2155a9abc5b53e1d753
Brando Ramello <brando.ramello@gmail.com>
2021-06-21 01:45:48 +0200

    Stop dei suoni durante le cutscenes (non i passi per qualche motivo). Il suono di jump non si smuta dopo acquisizione del quarto ricordo. Abbassata voce greca nel vento.

---
+++ ProgettoGD/Assets/MuteAudioSources.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuteAudioSources : MonoBehaviour
+{
+
+    [SerializeField] GameObject _videoPanel;
+    [SerializeField] AudioSource _player;
+    [SerializeField] AudioSource _ricordo;
+    [SerializeField] AudioSource _musica;
+    [SerializeField] AudioSource _ambientale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_videoPanel.activeSelf)
+        {
+            _player.mute = true;
+            _ricordo.mute = true;
+            _musica.mute = true;
+            _ambientale.mute = true;
+
+        }
+    }
+}
