c81eb77a0568706ca61a789f6d28c6bd21d99866
Brando Ramello <brando.ramello@gmail.com>
2021-06-24 12:10:07 +0200

    Aggiunto suono Splash, aggiunto easter egg, riattivato level loader Montagna

---
+++ ProgettoGD/Assets/Scripts/Splash.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Splash : MonoBehaviour
+{
+    [SerializeField] AudioSource _splashSound;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("SPLASH");
+            _splashSound.Play();
+        }
+    }
+}
